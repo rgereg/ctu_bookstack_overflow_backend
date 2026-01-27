9d430320523ffc81811ba9fae643c8d8692a7732
dreadlordshark <bloodiestshark@yahoo.com>
2026-01-27 00:59:30 -0500

    Update main.py
    dots more dots

--- main.py
+++ main.py
@@ -203,10 +203,10 @@ def update_quantity(data: UpdateQuantity, user=Depends(get_current_user)):
     role = user.user_metadata.get("role", "customer")
     if role != "employee":
         raise HTTPException(status_code=403, detail="Forbidden")
-    supabase.table("books").select("isbn").eq("isbn", data.isbn).execute() # DEBUG SECTION TODO REMOVE
-    supabase.table("books").update({"quantity": data.quantity}).eq("isbn", data.isbn).execute()  # DEBUG SECTION TODO REMOVE
+    result3 = supabase.table("books").select("isbn").eq("isbn", data.isbn).execute() # DEBUG SECTION TODO REMOVE
+    result2 = supabase.table("books").update({"quantity": data.quantity}).eq("isbn", data.isbn).execute()  # DEBUG SECTION TODO REMOVE
     result = supabase.table("books").update({"quantity": data.quantity}).eq("isbn", data.isbn).execute()
-    return {"status": "success", "data": result.data}
+    return {"status": "success", "data": result.data "data2": result2.data "data3": result3.data}
 
 @app.post("/update_price")
 def update_price(data: UpdatePrice, user=Depends(get_current_user)):
