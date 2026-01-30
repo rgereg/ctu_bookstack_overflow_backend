d38be86f5ebb3cee8fd4fc8c14433f682a7fe1bf
Tommy-Guilmartin <tjgiv99@gmail.com>
2026-01-30 04:52:50 -0500

    Trying to select from order_items table

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def get_cart(user=Depends(get_current_user), sb=Depends(get_supabase_authed)):
     userID = user.id
 
     # Search order table by userID, table added here
-    result = sb.table("orders").select("order_items(book_id)", "order_items(quantity)", "order_items(unit_price)").eq("customer_id", userID).eq("order_id", orderID).execute()
+    result = sb.table("order_items").select("*").execute()
     return result.data or []
 
 @app.get("/orders")
