b85670c11f792df4f10ea3c3cb496eab1d0f236d
Tommy-Guilmartin <tjgiv99@gmail.com>
2026-02-01 02:30:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def add_to_cart(cartData: CartAdd, user = Depends(get_current_user), sb = Depend
     
     # Find book by isbn and insert book id and amount to the order items table under order id
     book = sb.table("books").select("id").eq("isbn", cartData.isbn).execute()
-    result = sb.table("order_items").insert({"order_id": order_id, "book_id": book.id, "quantity": cartData.quantity})
+    result = sb.table("order_items").insert({"order_id": order_id, "book_id": book.id, "quantity": cartData.quantity}).execute()
     return result.data or []
 
 @app.get("/orders")
