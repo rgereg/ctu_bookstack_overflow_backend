f5749d550f8e7b87a5792a8dc887c4e5a9074379
Tommy-Guilmartin <tjgiv99@gmail.com>
2026-02-02 04:05:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -314,9 +314,9 @@ def add_to_cart(cartData: CartAdd, user=Depends(get_current_user), sb=Depends(ge
     checkBook = sb.table("order_items").select("*").eq("book_id", bookId).execute()
     if len(checkBook.data) != 0:
         newQuantity = cartData.quantity + checkBook.data[0]["quantity"]
-        sb.table("order_items").update({"quantity": newQuantity}).eq("order_id", cartOrderId).eq("book_id", bookId)
+        sb.table("order_items").update({"quantity": newQuantity}).eq("order_id", cartOrderId).eq("book_id", bookId).execute()
     else:
-        sb.table("order_items").insert({"order_id": cartOrderId, "book_id": bookId, "quantity": cartData.quantity, "unit_price": bookPrice})
+        sb.table("order_items").insert({"order_id": cartOrderId, "book_id": bookId, "quantity": cartData.quantity, "unit_price": bookPrice}).execute()
     
     return {"status": "Book added to cart"}
 
