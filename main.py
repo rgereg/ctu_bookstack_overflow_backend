ef9c6a88b01c8fcc8f414eaab2b26557f10d8793
Tommy-Guilmartin <tjgiv99@gmail.com>
2026-02-02 00:43:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -268,8 +268,11 @@ commented code works but is most basic, testing upgrades above
 
 @app.get("/cart")
 def get_cart(user=Depends(get_current_user), sb=Depends(get_supabase_authed)):
-    # More attempts to filter only cart items
-    items = sb.table("order_items").select("*, orders(*), books(*)").eq("orders.type", "cart").execute()
+    # Line below this can call all information from other tables, for some reason the orders table often returns as null
+    # items = sb.table("order_items").select("*, orders(*), books(*)").eq("orders.type", "cart").execute()
+    # Below attempts to search the orders table first to find cart order ids, and then pulls all order_items and books data
+    cartOrders = sb.table("orders").select("*").eq("type", "cart").execute()
+    items = sb.table("order_items").select("*, books(*)").eq("order_id", cartOrders["id"]).execute()
     return items.data or []
 
 
